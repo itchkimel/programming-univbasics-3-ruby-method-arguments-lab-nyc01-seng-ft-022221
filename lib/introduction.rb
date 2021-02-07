0c4d0e0962f03581f2990d98379e1176c53dadad
scrappy-cyborg-4243 <itchkimel@gmail.com>
2021-02-07 17:00:40 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,14 @@ def introduction(name)
   puts "Hi, my name is #{name}."  
 end
 
+def introduction(name)
+  puts "Hi, my name is #{name}."  
+end
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name}."  
+end
+
 
 
 
